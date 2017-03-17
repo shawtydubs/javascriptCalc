d65828f7dd94ed0ca31071f55e8ed1deb0b0d181
Claire Walters <claire.walters@opower.com>
2017-03-17 16:32:44 -0400

    NOREF create javascript calculator for free code camp

---
+++ calc.js
@@ -0,0 +1,64 @@
+angular.module('calcApp', []).controller('numController', function($scope) {
+    $scope.displayNum = [];
+    $scope.numTotal = 0;
+
+    $scope.makeNum = function(value) {
+        $scope.displayNum.push(value);
+    }
+
+    $scope.addNum = function() {
+        performOperation();
+        $scope.operation = '+';
+    }
+
+    $scope.subNum = function() {
+        performOperation();
+        $scope.operation = '-';
+    }
+
+    $scope.multNum = function() {
+        performOperation();
+        $scope.operation = '*';
+    }
+
+    $scope.divNum = function() {
+        performOperation();
+        $scope.operation = '/';
+    }
+
+    $scope.equals = function() {
+        performOperation();
+        $scope.operation = '=';
+    }
+
+    $scope.clearOne = function() {
+        $scope.displayNum = [];
+    }
+
+    $scope.allClear = function() {
+        $scope.numTotal = 0;
+        $scope.operation = undefined;
+        $scope.displayNum = [];
+    }
+
+    performOperation = function() {
+        if ($scope.numTotal == 0 || $scope.operation == '=') {
+            $scope.numTotal = parseFloat($scope.displayNum.join(''));
+        } else {
+            $scope.numTotal = calculateTotal($scope.numTotal, parseFloat($scope.displayNum.join('')));
+        }
+        $scope.displayNum = [];
+    }
+
+    calculateTotal = function(numOne, numTwo) {
+        if ($scope.operation == '+') {
+            return numOne + numTwo;
+        } else if ($scope.operation == '-') {
+            return numOne - numTwo;
+        } else if ($scope.operation == '*') {
+            return numOne * numTwo;
+        } else if ($scope.operation == '/') {
+            return numOne / numTwo;
+        }
+    }
+});
